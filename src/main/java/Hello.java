28a0ce27a366ae414d550f67f180fddd3ea208de
yangshaonsn <yangsn6@lenovo>
2021-06-21 17:02:36 +0800

    test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("hello Jenkins!123");
-        System.out.println("hello Jenkins!");
+        System.out.println("hello Jenkins!111");
+        System.out.println("hello Jenkins!222");
+        System.out.println("hello Jenkins!333");
 
     }
 
